98d9e66a35c9b06116272d72d795ed9867ab9ac5
Jhon Michael <89868237+DarkKing516@users.noreply.github.com>
2023-11-14 18:40:24 -0500

    Delete express-mongo-api directory

--- express-mongo-api/src/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-// controllers/indexController.js
-const indexController = {
-    getIndex: (req, res) => {
-      res.send('Welcome to the homepage!');
-    }
-  };
-  
-  module.exports = indexController;
-  
